a02efb929c7f4dfcad64071760c0a9bd45ff97f0
alainDuguine <alain.duguine@gmail.com>
2020-02-20 17:16:55 +0100

    Tests corrected

---
+++ Library-api/library-api-consumer/src/main/java/org/alain/library/api/consumer/repository/ReservationStatusRepository.java
@@ -0,0 +1,8 @@
+package org.alain.library.api.consumer.repository;
+
+
+import org.alain.library.api.model.reservation.ReservationStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationStatusRepository extends JpaRepository<ReservationStatus, Long> {
+}
