82e60446b46d27fb70d922f31b62822362fc144d
alainDuguine <alain.duguine@gmail.com>
2020-02-25 08:37:50 +0100

    Adding profile and directory structure for integration tests

---
+++ Library-api/library-api-service/src/integration-test/java/org/alain/library/api/service/api/LibraryApiServiceApplicationIT.java
@@ -0,0 +1,16 @@
+package org.alain.library.api.service.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class LibraryApiServiceApplicationIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
