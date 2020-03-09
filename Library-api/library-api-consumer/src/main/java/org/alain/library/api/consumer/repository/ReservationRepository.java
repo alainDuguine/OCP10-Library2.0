d6dc0ab19d54d37d61852f2b7216757141c9a300
alainDuguine <alain.duguine@gmail.com>
2020-03-09 07:40:06 +0100

    cleaning versions in poms

--- Library-api/library-api-consumer/src/main/java/org/alain/library/api/consumer/repository/ReservationRepository.java
+++ Library-api/library-api-consumer/src/main/java/org/alain/library/api/consumer/repository/ReservationRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     @Query("Select r from Reservation r WHERE (:status is null or r.currentStatus = :status)" +
             "and (:bookId is null or r.book.id = :bookId) and (:userId is null or r.user.id = :userId)")
     List<Reservation> findByCurrentStatusAndUserIdAndBookId(@Param("status") String status,@Param("bookId") Long bookId,@Param("userId") Long userId);
+
+    @Query("SELECT r FROM Reservation r WHERE (r.currentStatus = :status) AND (r.currentStatusDate > :expirationDate)")
+    List<Reservation> findByStatusExpired(@Param("status") String status, @Param("expirationDate") LocalDateTime expirationDate);
 
 }
